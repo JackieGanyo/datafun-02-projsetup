7423bc1c501b44cd234c6e29d3c2e0ed4b0a4e30
Jackie Ganyo <jacquelynganyo@gmail.com>
2024-05-10 12:51:11 -0500

    Fixed

--- start_project.py
+++ start_project.py
@@ -2,7 +2,8 @@
 
 import pathlib
 
-def create_project_directory(directory_name: str)
+def create_project_directory(directory_name: str):
+    
     """
     Creates a project sub directory
     : param directory_name: Name to be created, et. "test"
@@ -12,9 +13,8 @@ def create_project_directory(directory_name: str)
 
 def main(): 
     """Scaffold a project."""
-    create_project_directory(diurectory_name='test') #name the parameter
+    create_project_directory(directory_name='test') #name the parameter
     create_project_directory(directory_name='docs') #name the parameter
 
 if __name__ =='__main__':
     main()
-    
